6c934cde0719ed623929094f160ddda51d2d531d
skywind3000 <skywind3000@gmail.com>
2020-03-20 21:12:04 +0800

    update

--- queen/bench.sh
+++ queen/bench.sh
@@ -1,9 +1,9 @@
 #! /bin/sh
-echo "queen.c"
+echo "queen.c - non-recursion"
 gcc -O3 queen.c -o a.out && sleep 1 && ./a.out && rm -f a.out
-echo "queen.rs"
+echo "queen.rs - non-recursion"
 rustc -O -C opt-level=3 queen.rs -o a.out && sleep 1 && ./a.out && rm -f a.out
-echo "queen.go"
+echo "queen.go - non-recursion"
 go build -o a.out queen.go && sleep 1 && ./a.out && rm -f a.out
 echo "queen2.c - recursion"
 gcc -O3 queen2.c -o a.out && sleep 1 && ./a.out && rm -f a.out
