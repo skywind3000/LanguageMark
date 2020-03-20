ddb8f05dcb2b70dbc751f5cf7175a2d93b30dd7c
skywind3000 <skywind3000@gmail.com>
2020-03-20 20:36:56 +0800

    update

--- queen/bench.sh
+++ queen/bench.sh
@@ -1,3 +1,5 @@
 #! /bin/sh
-gcc -O3 queen-c.c -o queen-c
-rustc -O -C opt-level=3 queen-rs.rs -o queen-rs
+echo "queen.c"
+gcc -O3 queen.c -o a.out && sleep 1 && ./a.out && rm -f a.out
+echo "queen.rs"
+rustc -O -C opt-level=3 queen.rs -o a.out && sleep 1 && ./a.out && rm -rf a.out
